6ede0384955c56a30a028f8eb73e15496e1c93de
Ashu002 <ashu.ojha2010@gmail.com>
2016-11-19 10:33:55 +0530

    Modify listen port

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.get("*", function(req, res){
 });*/
 
 // After Socket
-http.listen(3000, function(err) {
+http.listen(process.env.PORT || 3000, function(err) {
 	if(err){
 		console.log(err);
 	}else{
